50d193e6bfba509cf13ff01ad38ea42d2d5a859e
AnastasiiaMikharieva <85961200+AnastasiiaMikharieva@users.noreply.github.com>
2022-01-12 17:49:25 -0500

    Reversed String

---
+++ src/main/java/Reversed.java
@@ -0,0 +1,19 @@
+public class Reversed {
+
+    public static void main(String[] args) {
+
+
+
+        String str = "Winner";
+
+        String reversed = "";
+
+        for (int i = str.length()-1; i >=0 ; i--) {
+
+            reversed+= str.charAt(i);
+
+        }
+
+        System.out.println(reversed);
+    }
+}
